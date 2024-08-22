0f3ada6fb0947b6066184a14e2196b6a740dcf41
Liliya Kulishova <lilyusha-kul@yandex.ru>
2024-08-22 10:20:33 +0300

    module 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ');
+  let mapped = arr.map(num => +num);
+  let newArr = [];
+  for (let ar of mapped) {
+    if (Number.isFinite(ar)) {
+    newArr.push(ar);
+    }
+  }
+  let mini = newArr.reduce((minim, current) => Math.min(minim, current));
+  let maxi = newArr.reduce((maxim, current) => Math.max(maxim, current));
+  let result = {min: mini, max: maxi};
+  return result;
 }
