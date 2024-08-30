62bd75eb9f669024deeaa817fc95da6948e230b7
Liliya Kulishova <lilyusha-kul@yandex.ru>
2024-08-30 12:02:41 +0300

    module 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,6 @@
 function hideSelf() {
-  // ваш код...
+  let elem = document.querySelector('.hide-self-button');
+  elem.onclick = function () {
+    elem.hidden = true;
+  }
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
-  // ваш код...
+  let elem = document.querySelector('.toggle-text-button');
+  let text = document.getElementById('text');
+  elem.onclick = function () {
+    if (!text.hidden) {
+      text.hidden = true;
+    } else {
+      text.hidden = false;
+    }
+  } 
 }
