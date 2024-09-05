4538c9b12483ba7a108d4b4d4e0634e31543d804
Liliya Kulishova <lilyusha-kul@yandex.ru>
2024-09-05 14:47:23 +0300

    module 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,66 @@
  *
  */
 export default class UserTable {
+  elem = null; 
+  rows = []; 
+ 
   constructor(rows) {
+    this.rows = rows;
+    
+    this.elem = document.createElement('table');
+    let thead = document.createElement('thead');
+    let name = document.createElement('th');
+    name.textContent = 'Имя';
+    thead.appendChild(name);
+    
+    let age = document.createElement('th');
+    age.textContent = 'Возраст';
+    thead.appendChild(age);
+
+    let salary = document.createElement('th');
+    salary.textContent = 'Зарплата';
+    thead.appendChild(salary);
+
+    let city = document.createElement('th');
+    city.textContent = 'Город';
+    thead.appendChild(city);
+
+    this.elem.appendChild(thead);
+
+    this.changeArr();
   }
+
+  changeArr() {
+    let tbody = document.createElement('tbody');
+    for (let row of this.rows) {
+      let tr = document.createElement('tr');
+      let td = document.createElement('td');
+      td.textContent = row.name;
+      tr.appendChild(td);
+
+      let td2 = document.createElement('td');
+      td2.textContent = row.age;
+      tr.append(td2);
+     
+      let td3 = document.createElement('td');
+      td3.textContent = row.salary;
+      tr.append(td3);
+
+      let td4 = document.createElement('td');
+      td4.textContent = row.city;
+      tr.append(td4);
+
+      let td5 = document.createElement('td');
+      td5.innerHTML = '<button>X</button>';
+      tr.append(td5);
+
+      tbody.appendChild(tr);
+
+      td5.onclick = function(){
+        td5.parentNode.remove();
+      }  
+    }
+    this.elem.appendChild(tbody);
+  } 
+
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,116 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+  elem = null;
+
+  arrowRight = null;
+  arrowLeft = null;
+  inner = null;
+  slide = null;
+    
+  currentSlide = 0;
+  slidesCount = null;
+
   constructor(slides) {
     this.slides = slides;
+  
+    this.elem = this.render();
+
+    this.arrowRight = this.elem.querySelector('.carousel__arrow_right');
+    this.arrowLeft = this.elem.querySelector('.carousel__arrow_left');
+    this.inner = this.elem.querySelector('.carousel__inner'); 
+    this.slide = this.elem.querySelector('.carousel__slide');
+      
+    this.currentSlide = 0;
+    this.slidesCount = this.elem.querySelectorAll('.carousel__slide').length - 1; 
+    
+    this.selfDisplayControl();
+    this.arrowRight.addEventListener("click", this.transformRight);
+    this.arrowLeft.addEventListener("click", this.transformLeft);
+
+    this.generateEvents();
   }
+
+  generateEvents() {
+    for (let slide of this.slides) {
+      let slideButton = this.elem.querySelector(`[data-id="${slide.id}"]`);
+      slideButton.addEventListener('click', () => {
+        this.elem.dispatchEvent (new CustomEvent("product-add", {
+          detail: slide.id, 
+          bubbles: true 
+        }));
+      })
+    }
+  }
+
+  selfDisplayControl() {
+    if (this.currentSlide == this.slidesCount) {
+      this.arrowRight.style.display = 'none';
+      this.arrowLeft.style.display = '';
+    } else if (this.currentSlide == 0) {
+      this.arrowLeft.style.display = 'none'; 
+      this.arrowRight.style.display = '';
+    } else {
+      this.arrowRight.style.display = '';
+      this.arrowLeft.style.display = '';
+    }
+  }
+
+  transformRight = () => {
+    this.currentSlide = this.currentSlide + 1;
+    let currentOffset = this.slide.offsetWidth * this.currentSlide;
+    this.inner.style.transform = `translateX(-${currentOffset}px)`;
+    this.selfDisplayControl();
+  }
+  
+  transformLeft = () => {
+    this.currentSlide = this.currentSlide - 1;
+    let currentOffset = this.slide.offsetWidth * this.currentSlide;
+    this.inner.style.transform = `translateX(-${currentOffset}px)`;
+    this.selfDisplayControl();
+  }
+
+  template() {
+    return `
+      <!--Корневой элемент карусели-->
+      <div class="carousel">
+        <!--Кнопки переключения-->
+        <div class="carousel__arrow carousel__arrow_right">
+          <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+        </div>
+        <div class="carousel__arrow carousel__arrow_left">
+          <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+        </div>
+
+        <div class="carousel__inner">
+          ${this.generateSliderItems()}
+        </div>
+      </div>
+    `}
+    
+    generateSliderItems() {
+      let result = '';
+      for (let slide of this.slides) {
+        result += `
+          <div class="carousel__slide" data-id=${slide.id}>
+            <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+            <div class="carousel__caption">
+              <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+              <div class="carousel__title">${slide.name}</div>
+              <button type="button" class="carousel__button">
+                <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+              </button>
+            </div>
+          </div>
+        `
+      }
+      return result;
+    }
+
+    render() {
+      return createElement(this.template());
+    }  
 }
+
+  
+
